e0bdb0dc6d8293d5db3d870ad0265c41d6b86f5e
caleosun123 <osunagarciacaleb@gmail.com>
2025-02-24 09:35:13 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+  "database/sql"
+  "fmt"
+  "log"
   "net/http"
   "html/template"
+  _ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
 func main() {
+
+  var err error
+  db, err = sql.Open("mysql", "sql5764415:nEIpTAV8Hj@tcp(sql5.freesqldatabase.com:3306)/sql5764415")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer db.Close()
+
+  err = db.Ping()
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println("Successfully connected to the database")
+  
   http.HandleFunc("/", helloHandler)
   http.ListenAndServe(":8080", nil)
 }
